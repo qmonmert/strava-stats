c4ec176d273c10bf60fb1d90f9c0139970745faf
Quentin Monmert <qmonmert@mbp-de-cedric.home>
2017-02-25 17:07:12 +0100

    Creating the library (src/index.js) and add JSON files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var athleteJSON = require('../json/athlete.json');
+var activitiesJSON = require('../json/activities.json');
+
+module.exports = {
+    athlete: athleteJSON,
+    activities: activitiesJSON
+}
